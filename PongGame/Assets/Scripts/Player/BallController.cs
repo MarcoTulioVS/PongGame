003c0921bfbc7f11996d73ef3e767a616e25874e
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2024-02-09 17:34:44 -0300

    Adding ball collision
    When the ball collides with the wall, the enemy or the player it will be thrown to the opposite side.

--- PongGame/Assets/Scripts/Player/BallController.cs
+++ PongGame/Assets/Scripts/Player/BallController.cs
@@ -13,4 +13,19 @@ public class BallController : MonoBehaviour
     {
         rb.velocity = startingVelocity;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Vector2 newVelocity = rb.velocity;
+            newVelocity.y = -newVelocity.y;
+            rb.velocity = newVelocity;
+        }
+
+        if(collision.gameObject.tag=="Player" || collision.gameObject.tag == "Enemy")
+        {
+            rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+        }
+    }
 }
